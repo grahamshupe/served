df7e88bdf23bf42a28cecb2445d5c9242b5ab98b
minishupe <grahamshupe04@gmail.com>
2024-05-16 22:11:22 -0700

    Complete req_parse and its helper functions

--- src/http.c
+++ src/http.c
@@ -1,5 +1,10 @@
 /*
 Functions for creating and modifying HTTP requests and responses.
+
+TODO:
+- _req_parse_headers() should recognize multiple field lines (RFC 9110:5.2)
+- _req_parse helper functions should recognize both \r\n and \n as line endings
+- make headers a hashset, instead of a linked list.
 */
 
 #include <stdio.h>
@@ -8,76 +13,132 @@ Functions for creating and modifying HTTP requests and responses.
 #include "http.h"
 
 
-#define TARGET_SIZE 1024
+/*
+ * Private Functions *
+*/
 
-
-int get_token(const char* message, char delim, int size) {
-    int i = 0;
-    while (message[i] != delim && i < size) {
-        i++;
+size_t memcspn(const char* str, const char* reject, size_t str_size) {
+    size_t count = 0;
+    size_t reject_len = strlen(reject);
+    for (; count < str_size; count++) {
+        for (size_t i = 0; i < reject_len; i++) {
+            if (reject[i] == str[count]) {
+                return count;
+            }
+        }
     }
-    return i;
+    return count;
 }
 
-
-int req_parse_start(char* message, request_t* request) {
-    char* msg_ptr = message;
+int _req_parse_start(char* message, size_t* size, struct request* req) {
     // Get method:
-    if (strncmp(message, "GET ", 4)) {
-        request->method = GET;
-        msg_ptr + 4;
+    if (*size < 16) {
+        // startline can't possibly be long enough
+        return 400;
+    } else if (strncmp(message, "GET ", 4)) {
+        req->method = GET;
+        message += 4;
+        *size -= 4;
     } else if (strncmp(message, "HEAD ", 5)) {
-        request->method = HEAD;
-        msg_ptr + 5;
+        req->method = HEAD;
+        message += 5;
+        *size -= 5;
     } else if (strncmp(message, "POST ", 5)) {
-        request->method = POST;
-        msg_ptr + 5;
+        req->method = POST;
+        message += 5;
+        *size -= 5;
     } else {
         // invalid method
         return 501;
     }
 
     // Get target:
-    int token_size = get_token(msg_ptr, ' ', TARGET_SIZE);
-    if (token_size == 0) {
-        // no target or too many spaces given
-        return 400;
-    } else if (token_size == TARGET_SIZE) {
-        // target is too long
-        return 414;
-    }
-    request->target = (char*) malloc(token_size + 1);
-    strncpy(request->target, msg_ptr, token_size);
-    request->target[token_size] = '\0';
+    size_t token_size = memcspn(message, " ", *size);
+    if (token_size == 0)
+        return 400;  // no target or too many spaces given
+    if (token_size == *size)
+        return 414;  // target is too long
+
+    req->target = (char*) malloc(token_size + 1);
+    strncpy(req->target, message, token_size);
+    req->target[token_size] = '\0';
+    message += token_size + 1;
+    *size -= token_size + 1;
 
     // Get protocol:
-    msg_ptr += token_size + 1;
-    if (strncmp(msg_ptr, "HTTP/1.1\r\n", 10) != 0) {
+    if (*size < 10 || strncmp(message, "HTTP/1.1\r\n", 10) != 0) {
         // invalid protocol (must be HTTP/1.1)
-        free(request->target);
-        request->target = NULL;
+        free(req->target);
+        req->target = NULL;
         return 400;
     }
-    strncpy(request->protocol, "HTTP/1.1", 9);
+    strncpy(req->protocol, "HTTP/1.1", 9);
 
+    message += 10;  // go to the end of the startline
+    *size -= 10;
     return 200;
 }
 
-int req_parse_headers(char* message, request_t* request) {
+int _req_parse_headers(char* message, size_t* size, struct request* req) {
+    size_t line_size = 0;
+    req->headers = NULL;
 
+    while ((line_size = memcspn(message, "\r", *size)) != 0) {
+        // Add header to end of the headers linked list:
+        struct header* header = malloc(sizeof(struct header));
+        if (req->headers == NULL) {
+            req->headers = header;
+        } else {
+            struct header* runner = req->headers;
+            while (runner->next != NULL) {
+                runner = runner->next;
+            }
+            runner->next = header;
+        }
+
+        // Get field name:
+        size_t name_size = memcspn(message, ":", line_size);
+        if (line_size == *size || name_size == line_size)
+            return 400;
+        header->name = (char*) malloc(name_size + 1);
+        strncpy(header->name, message, name_size);
+        header->name[name_size] = '\0';
+        message += name_size + 1;
+
+        // Get field value:
+        size_t ows = strspn(message, " ");
+        message += ows;  // ignore beginning optional white space
+        size_t val_size = memcspn(
+            message, " \r", line_size - (name_size + 1 + ows));
+        header->value = (char*) malloc(val_size + 1);
+        strncpy(header->value, message, val_size);
+        header->value[val_size] = '\0';
+        message += line_size - (name_size + 1 + ows + 2);
+        *size -= line_size + 2;
+    }
+
+    message += 2;
+    *size -= 2;
+    return 200;
 }
 
-int req_parse(char* message, request_t* request) {
-    int status = 0;
-    status = req_parse_start(message, request);
-    if (status != 200) {
-        return status;
-    }
-    status = req_parse_headers(message, request);
-    if (status != 200) {
-        return status;
-    }
+/*
+ * Public Functions *
+*/
 
+int req_parse(char* message, struct request* req, size_t msg_size) {
+    int status = 0;
+    if (status = _req_parse_start(message, &msg_size, req) != 200)
+        return status;
+    if (status = _req_parse_headers(message, &msg_size, req) != 200)
+        return status;
+
+    // Parse body:
+    req->body = (char*) malloc(msg_size + 1);
+    strncpy(req->body, message, msg_size);
+    req->body[msg_size] = '\0';
+
+    return 200;
 }
 
 void req_free() {

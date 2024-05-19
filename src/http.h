1f1a1d7f572184f06b0fb1def06033010db764c8
Graham Shupe <grahamshupe04@gmail.com>
2024-05-18 23:46:33 -0700

    Add resp_add_header(), resp_new(), and resp_to_str() delcarations and descriptions

--- src/http.h
+++ src/http.h
@@ -26,7 +26,7 @@ struct request {
 struct response {
     char protocol[9];
     int status;
-    char* message;
+    char reason[32];
     struct header* headers;
     char* body;
 };
@@ -39,7 +39,7 @@ Unlike strcspn(), this only reads a max of SIZE bytes.
 size_t memcspn(const char* str, const char* reject, size_t str_size);
 
 /*
-Creates a new HTTP request from the given HTTP message.
+Parses a HTTP message into a HTTP request.
 On success, REQ is filled and 200 is returned.
 On failure, the HTTP status code of the error is returned.
 */
@@ -50,5 +50,22 @@ Free all malloc'd members in REQ.
 */
 void req_free(struct request* req);
 
+/*
+Fills the members of the given HTTP response.
+Generic headers will be added. Additional headers must be added after
+calling this function.
+*/
+void resp_new(struct response* resp, int status, char* body);
+
+/*
+Adds a new header to RESP with the given NAME and VALUE.
+*/
+void resp_add_header(struct response* resp, const char* name, 
+                    const char* value);
+
+/*
+Converts the given response into a string, ready to be sent as a HTTP message.
+*/
+void resp_to_str(struct response* resp, char* str);
 
 #endif

2499b359b8db7e618741462c6e7dfdcc7fe7bad0
Graham Shupe <grahamshupe04@gmail.com>
2024-05-27 19:14:26 -0700

    Implement basic functions for GET responses; server works

--- src/server.c
+++ src/server.c
@@ -7,8 +7,10 @@
 #include <stdlib.h>
 #include "http.h"
 
-#define PORT "80"
+#define PORT "3491"
 #define QUEUE_SIZE 32
+#define ROOT_PATH "root"
+#define BODY_SIZE 16384
 
 
 // Gets a listening socket for the server.
@@ -82,13 +84,39 @@ int get_socket() {
     return sock_fd;
 }
 
+// Handles a GET method request.
+int handle_get(struct response* resp, struct request* req) {
+    FILE* file = NULL;
+    // // Try to get file:
+    // char* path = malloc(strlen(req->target) + strlen(ROOT_PATH) + );
+    // strcpy(path, ROOT_PATH);
+    // strcat(path, req->target);
+    // strcat(path, )
+    // file = fopen(path, "r");
+    // if (file == NULL) {
+    //     free(path);
+    //     return 404;
+    // }
+
+    if (strcmp(req->target, "/") != 0)
+        return 404;
+    file = fopen("root/index.html", "r");
+    if (file == NULL)
+        return 500;
+    
+    resp->body = malloc(BODY_SIZE);
+    int size = fread(resp->body, 1, BODY_SIZE, file);
+    resp->body[size] = '\0';
+    return 200;
+}
+
 // Handles a http request from a client.
 // client_fd must be an open socket to connect to.
 void handle_connection(int client_fd) {
     char* message = malloc(REQUEST_SIZE);
     char* msg_origin = message;  // keep track of start so we can free msg
     ssize_t received = recv(client_fd, message, REQUEST_SIZE, 0);
-    
+
     if (received == -1) {
         perror("recv");
         free(message);
@@ -97,13 +125,31 @@ void handle_connection(int client_fd) {
 
     struct request req;
     int parse_status = req_parse(message, &req, received);
+    struct response resp;
     if (parse_status != 200) {
-        // send an error response
+        resp_new(&resp, parse_status, NULL);
+
+    } else {
+        switch (req.method) {
+            case GET:
+                resp_new(&resp, 200, NULL);
+                handle_get(&resp, &req);
+                break;
+            case HEAD:
+            case POST:
+            default:
+                resp_new(&resp, 501, NULL);
+        }
     }
-    // send a normal response
+
+    char resp_msg[BODY_SIZE];
+    int resp_size = resp_to_str(&resp, resp_msg);
+    printf("response:\n%s\n", resp_msg);
+    send(client_fd, resp_msg, resp_size, 0);
 
     req_free(&req);
-    
+    free(msg_origin);
+    msg_origin, message = NULL;
 }
 
 
@@ -117,6 +163,7 @@ int main(int argc, char* argv[]) {
         int client_fd = accept(sock_fd, NULL, NULL);
 
         // TODO: make this multi-threaded
+        puts("Served: got a connection");
         handle_connection(client_fd);
 
         close(client_fd);

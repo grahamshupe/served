831d90b7e0caf5544395d063a5a0964ad61f301d
Graham Shupe <grahamshupe04@gmail.com>
2024-08-02 00:45:32 -0700

    Add file

---
+++ src/http.c
@@ -0,0 +1,13 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "request.h"
+#include "response.h"
+#include "http.h"
+
+
+
+
---
+++ src/http.h
@@ -0,0 +1,7 @@
+#ifndef HTTP_H_
+#define HTTP_H_
+
+
+
+
+#endif
